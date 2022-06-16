b650f30423066d268f797fa89f03c6d7b4fae5db
justynas <staniszewskajustyn@gmail.com>
2022-06-16 19:19:20 +0200

    adding styles, suggestions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import TagsInput from "./components/TagsInput";
+import "./styles/style.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Enter Tags</h2>
+      <h2>ENTER TAGS</h2>
       <TagsInput />
     </div>
   );
